656998c1e5ba4581d58e67cbc01e8d27510c1f43
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2023-06-29 11:06:24 +0530

    Update lab.sh

--- lab.sh
+++ lab.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-DIR=/home/ubuntu/lab1
-FILE=/home/ubuntu/lab1/output
-container_name=SQL-Injection
-if [ -d "$DIR" ];
-then
-printf '%s\n' "SQL-Injection dir ($DIR)"
-rm -rf "$DIR"
-else
-echo "now no SQL-Injection a dir"
+DIR=./srikanth
+[ -d $DIR ] && echo "Directory Exists" || mkdir $DIR
 fi
-echo "cloning a SQL-Injection dir"
+cd ./srikanth/
+echo "cloning a SQL-Injection"
 sudo git clone https://github.com/srikanthreddy-lectures/SQL-Injection.git
-cd /home/ubuntu/lab1/SQL-Injection/
+cd ./srikanth/SQL-Injection/
 sudo ant -d clean compile dist
 result=$( sudo docker images -q sql-injection_web )
 if [[ -n "$result" ]]; then
@@ -21,8 +15,5 @@ sudo docker-compose down --volume --rmi all
 else
 echo "No such image"
 fi
-echo "change the dir"
-
-echo "delete output file"
 echo "build the docker image"
-sudo docker-compose up >> $FILE
+sudo docker-compose up -d
