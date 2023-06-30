284665ada09b28e987ce4d59b80e5e39669540a4
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2023-06-30 16:30:22 +0530

    Update LoginServlet.java

--- src/com/app/login/LoginServlet.java
+++ src/com/app/login/LoginServlet.java
@@ -11,7 +11,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
+
+import java.sql.DriverManager;
+
 
 import com.app.user.User;
 
