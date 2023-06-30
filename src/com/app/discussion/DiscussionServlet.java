2be77e9e4adb69a74a1326d1afb9dd495396c668
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2023-06-30 16:22:16 +0530

    Update DiscussionServlet.java

--- src/com/app/discussion/DiscussionServlet.java
+++ src/com/app/discussion/DiscussionServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
+//import javax.sql.DataSource;
 
 import com.app.todo.Todo;
 
@@ -22,8 +22,8 @@ import com.app.todo.Todo;
 public class DiscussionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	@Resource(name = "jdbc/demo1")
-    private DataSource ds;
+	//@Resource(name = "jdbc/demo1")
+   // private DataSource ds;
 	Connection conn;
 	
 	private DiscussionService discussionService = new DiscussionService();   
@@ -32,7 +32,17 @@ public class DiscussionServlet extends HttpServlet {
 		
 		try
 		{
-			conn = ds.getConnection();
+			// Initialize the database
+			  String dbDriver = "com.mysql.jdbc.Driver";
+		        String dbURL = "jdbc:mysql://db:3306/";
+		        // Database name to access
+		        String dbName = "demo1";
+		        String dbUsername = "srikanth";
+		        String dbPassword = "17121981";
+		  
+		        Class.forName(dbDriver);
+		        conn = DriverManager.getConnection(dbURL + dbName, dbUsername, dbPassword);
+			//conn = ds.getConnection();
 			request.setAttribute("messages", discussionService.retrieveMessage(conn));
 			request.getRequestDispatcher("view/discussion.jsp").forward(request, response);
 			
